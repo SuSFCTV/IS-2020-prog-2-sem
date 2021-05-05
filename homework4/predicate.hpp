86de34d29d352f8ae47bc9c1bcc0c5c7013a1d3c
Suslov Mikhail <48714345+SuSFCTV@users.noreply.github.com>
2021-05-05 22:54:30 +0500

    Add files via upload

---
+++ homework4/predicate.hpp
@@ -0,0 +1,131 @@
+//
+// Created by misha on 05.05.2021.
+//
+
+#ifndef HOMEWORK4_PREDICATE_HPP
+#define HOMEWORK4_PREDICATE_HPP
+#include <iostream>
+#include <sstream>
+#include <assert.h>
+#include <vector>
+#include <set>
+#include <iterator>
+#include <functional>
+
+
+using namespace std;
+
+template<typename T, typename P>
+bool allOf(const T begin,const  T end, const P &p) {
+    for (T i = begin; i != end; i++) {
+        if (!p(*i)) {
+            return false;
+        }
+    }
+    return true;
+};
+
+template<typename T, typename P>
+bool anyOf(const T &begin, const T &end, const P &p) {
+    for (T i = begin; i < end; i++) {
+        if (p(*i)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+template <typename T, typename P>
+bool noneOf (const T& begin, const T& end, P func) {
+    for (T i = begin; i != end; ++i) {
+        if (func(*i)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+template<typename T, typename P>
+bool oneOf(const T &begin, const T &end, const P &p) {
+    int count_of_agrees = 0;
+    for (T i = begin; i < end; i++) {
+        if (p(*i)) {
+            count_of_agrees++;
+        }
+    }
+
+    if (count_of_agrees == 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+template<typename T, typename P = less<>>
+bool isSorted(T begin, T end, P pred = P()) {
+    for (T i = begin; i < end - 1; i++) {
+        if (!pred(*i, *(i + 1))) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+template <typename T, typename P>
+bool isPartitioned (T first, T last, P pred) {
+    for (; first != last; ++first) {
+        if (!pred(*first))
+            break;
+    }
+    for (; first != last; ++first) {
+        if (pred(*first))
+            return false;
+    }
+    return true;
+}
+
+template<typename T1, typename T2>
+T1 findNot(const T1 &begin, const T1 &end, T2 value) {
+    for (T1 i = begin; i < end; i++) {
+        if (*i != value) {
+            return i;
+        }
+    }
+    return end;
+}
+
+template<typename T, typename F>
+T findBackward(T first, T second, const F &value) {
+    auto temp_value = second;
+    while (first != second) {
+        if (*first == value) {
+            temp_value = first;
+        }
+        first++;
+    }
+    return temp_value;
+}
+
+template<typename T, typename C>
+bool isPalindrome(T first, T last, C comp){
+    if (first == last){
+        return true;
+    }
+    --last;
+    while (last != first){
+        if(!comp(*first,*last)){
+            return false;
+        }
+        ++first;
+
+        if(first == last){
+            break;
+        }
+        --last;
+    }
+    return true;
+}
+
+
+#endif //HOMEWORK4_PREDICATE_HPP
