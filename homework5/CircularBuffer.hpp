8c0b22e2e08a2c8beaca2413260a5b4da7c3b131
Suslov Mikhail <48714345+SuSFCTV@users.noreply.github.com>
2021-05-31 19:08:51 +0500

    Add files via upload

--- homework5/CircularBuffer.hpp
+++ homework5/CircularBuffer.hpp
@@ -1,332 +1,280 @@
 #include <iostream>
 #include <exception>
 #include <iterator>
-
+#include <string>
 using namespace std;
-//todo O(1)
-//todo there is already min in c++
+//fixed O(1)
 #define min(a, b) ((a < b) ? a : b)
-
 template<class T>
-class Iterator : iterator<random_access_iterator_tag, T> {
-    T *curr;
+class Iterator  {
+    T* array;
+    int start;
+    int capacity;
+    int curr;
 public:
-    using typename iterator<random_access_iterator_tag, T>::iterator_category;
-    using typename iterator<random_access_iterator_tag, T>::value_type;
-    using typename iterator<random_access_iterator_tag, T>::difference_type;
-    using typename iterator<random_access_iterator_tag, T>::pointer;
-    using typename iterator<random_access_iterator_tag, T>::reference;
+    using iterator_category = random_access_iterator_tag;
+    using value_type = T;
+    using difference_type = ptrdiff_t;
+    using pointer = T*;
+    using reference = T&;
 
-    Iterator() {}
+    Iterator() { }
 
-    Iterator(T *cur_pos)
-            : curr(cur_pos) {}
+    Iterator(T* array_, int start_, int capacity_, int cur_pos)
+            : array(array_), start(start_), capacity(capacity_), curr(cur_pos) { }
 
-    Iterator(const Iterator &iter)
-            : curr(iter.curr) {}
+    Iterator(const Iterator& iter)
+            : array(iter.array), start(iter.start), capacity(iter.capacity), curr(iter.curr) { }
 
-    T &operator*() {
-        return *curr;
+    T& operator*()
+    {
+        return array[(curr + start) % capacity];
     }
 
-    T *operator->() {
-        return curr;
+    T* operator->()
+    {
+        return &curr;
     }
 
-    Iterator &operator++() {
+    Iterator& operator++()
+    {
         curr++;
         return *this;
     }
 
-    Iterator operator++(int) {
+    Iterator operator++(int)
+    {
         Iterator tmp(*this);
         ++tmp;
         return tmp;
     }
 
-    Iterator &operator--() {
+    Iterator& operator--()
+    {
         curr--;
         return *this;
     }
 
-    Iterator operator--(int) {
+    Iterator operator--(int)
+    {
         Iterator tmp(*this);
         --tmp;
         return tmp;
     }
 
-    Iterator operator-(difference_type iter) {
-        return Iterator(curr - iter);
+    Iterator operator-(Iterator::difference_type iter)
+    {
+        return Iterator(array, start, capacity, curr - iter);
     }
 
-    Iterator operator+(difference_type iter) {
-        return Iterator(curr + iter);
+    Iterator operator+(Iterator::difference_type iter)
+    {
+        return Iterator(array, start, capacity, curr + iter);
     }
 
-    bool operator==(const Iterator &lhs) const {
+    bool operator==(const Iterator& lhs) const
+    {
         return lhs.curr == curr;
     }
 
-    bool operator==(T *pointer) const {
-        return pointer == curr;
-    }
-
-    bool operator!=(const Iterator &lhs) const {
+    bool operator!=(const Iterator& lhs) const
+    {
         return lhs.curr != curr;
     }
 
-    bool operator!=(T *pointer) const {
-        return pointer != curr;
+    bool operator==(T* pointer) const
+    {
+        return *pointer == curr;
     }
 
-    bool operator>(const Iterator &lhs) const {
+    bool operator!=(T* pointer) const
+    {
+        return *pointer != curr;
+    }
+
+    bool operator>(const Iterator& lhs) const
+    {
         return curr > lhs.curr;
     }
 
-    bool operator<(const Iterator &lhs) const {
+    bool operator<(const Iterator& lhs) const
+    {
         return curr < lhs.curr;
     }
 
-    bool operator>=(const Iterator &lhs) const {
+    bool operator>=(const Iterator& lhs) const
+    {
         return curr >= lhs.curr;
     }
 
-    bool operator<=(const Iterator &lhs) const {
+    bool operator<=(const Iterator& lhs) const
+    {
         return curr <= lhs.curr;
     }
 
-    bool operator>(T *pointer) const {
-        return curr > pointer;
-    }
-
-    bool operator<(T *pointer) const {
-        return curr < pointer;
-    }
-
-    bool operator>=(T *pointer) const {
-        return curr >= pointer;
-    }
-
-    bool operator<=(T *pointer) const {
-        return curr <= pointer;
-    }
-
-    Iterator &operator+=(difference_type iter) {
+    Iterator& operator+=(Iterator::difference_type iter)
+    {
         curr += iter;
         return *this;
     }
 
-    Iterator &operator-=(difference_type iter) {
+    Iterator& operator-=(Iterator::difference_type iter)
+    {
         curr -= iter;
         return *this;
     }
 
-    Iterator &operator=(const Iterator &lhs) {
+    Iterator& operator=(const Iterator& lhs)
+    {
         curr = lhs.curr;
+        start = lhs.start;
+        capacity = lhs.capacity;
+        array = lhs.array;
         return *this;
     }
 
-    Iterator &operator=(Iterator *pointer) {
-        curr = pointer;
-        return *this;
-    }
-
-    friend Iterator operator+(Iterator::difference_type n, Iterator iter) {
+    friend Iterator operator+(Iterator::difference_type n, Iterator iter)
+    {
         return iter + n;
     }
 
-    friend Iterator::difference_type operator-(const Iterator &lhs, const Iterator &rhs) {
+    friend Iterator::difference_type operator-(const Iterator& lhs, const Iterator& rhs)
+    {
         return lhs.curr - rhs.curr;
     }
 
 };
 
 
+
 template<class T>
 class CircularBuffer {
-    unsigned size;
-    unsigned capacity;
-    T *array;
-    Iterator<T> begin_;
-    Iterator<T> end_;
 public:
-    CircularBuffer(unsigned size_) {
-        capacity = size_;
-        size = 0;
+    int size_;
+    int capacity;
+    T* array;
+    int head;
+
+    CircularBuffer(int capacity_)
+    {
+        capacity = capacity_;
         array = new T[capacity];
+        head = 0;
+        size_ = 0;
+    }
+
+    void addFirst(T value_)
+    {
+        if (head == 0){
+            head = capacity - 1;
+        }
+
+        else{
+            head--;
+        }
+
+        array[head] = value_;
+        if (size_ < capacity){
+            size_++;
+        }
+
+    };
+
+    void addLast(T value_)
+    {
+        array[(head + size_) % capacity] = value_;
+        if (size_ < capacity){
+            size_++;
+        }
+    }
+
+    void delFirst()
+    {
+
+        head = (head + 1) % capacity;
+        if (size_ > 0){
+            size_--;
+        }
+
 
     }
 
-    Iterator<T> begin() const {
-        return Iterator(array);
+    void delLast()
+    {
+        if (size_ > 0){
+            size_--;
+        }
+
     }
 
-    Iterator<T> end() const {
-        return Iterator(array + size);
+    T& first()
+    {
+        if (size_ == 0){
+            throw range_error("Empty buffer");
+        }
+        return array[head];
     }
 
-    void addFirst(T data_) {
-        if (capacity > 0) {
-            if (size < capacity) {
-                if (size == 0) {
-                    array[size] = data_;
-                    size++;
+    T& last()
+    {
+        if (size_ == 0){
+            throw range_error("Empty buffer");
+        }
+        return array[(head + size_ - 1) % capacity];
+    }
 
-                } else {
-                    begin_ = begin();
-                    end_ = end() - 1;
-                    for (Iterator<T> iter = end_; iter >= begin_; --iter) {
-                        *(iter + 1) = *iter;
+    Iterator<T> begin() const
+    {
+        return Iterator(array, head % capacity, capacity, 0);
+    }
 
-                    }
+    Iterator<T> end() const
+    {
+        return Iterator(array, head % capacity, capacity, size_);
 
-                    array[0] = data_;
-                    size++;
+    }
 
-                }
-            } else {
-                size = capacity - 1;
-                begin_ = begin();
-                end_ = end();
-                for (Iterator<T> iter = end_; iter >= begin_; --iter) {
-                    *(iter + 1) = *iter;
+    //fixed more informations in exceptions
+    T& operator[](int index)
+    {
+        if (index >= capacity){
+            throw range_error("Invalid value index. Index is bigger than capacity. Capacity is " + to_string(capacity) + " | Index is " +  to_string(index + 1));
+        }
+        else if (index < capacity && index >= size_ ){
+            throw range_error("Invalid value index. Not enough elems in the buffer, there are " + to_string(size_) + " elems in buffer");
+        }
+        return array[(head + index) % capacity];
+    };
 
-                }
+    T& operator[](int index) const
+    {
+        if (index >= capacity){
+            throw range_error("Invalid value index. Index is bigger than capacity. Capacity is " + to_string(capacity) + "| Index is " +  to_string(index + 1));
+        }
+        else if (index < capacity && index >= size_ ){
+            throw range_error("Invalid value index. Not enough elems in the buffer, there are " + to_string(size_) + " elems in buffer");
+        }
+        return array[(head + index) % capacity];
+    };
 
-                array[0] = data_;
-                size++;
-
+    void changeCapacity(int capacity_)
+    {
+        if (capacity_ > 0) {
+            int new_capacity = capacity_;
+            int new_size = min(new_capacity, size_);
+            T* newArray = new T[new_capacity]();
+            for (int i = 0; i < new_size; i++){
+                newArray[i] = array[head];
+                ++head %= capacity;
             }
 
-        } else {
-            throw range_error("Negative/0 capacity");
-
-        }
-    }
-
-    void delFirst() {
-        if (size > 0) {
-            begin_ = begin();
-            end_ = end();
-            for (Iterator<T> iter = begin_ + 1; iter <= end_; ++iter) {
-                *(iter - 1) = *iter;
-            }
-            size--;
-
-        } else {
-            throw range_error("Empty buffer");
-
-        }
-    }
-
-    void addLast(T data_) {
-        if (capacity > 0) {
-            if (size < capacity) {
-                if (size == 0) {
-                    array[size] = data_;
-                    size++;
-
-                } else {
-                    array[size] = data_;
-                    size++;
-
-                }
-            } else {
-                size = capacity - 1;
-                begin_ = begin();
-                end_ = end();
-                for (Iterator<T> iter = begin_ + 1; iter <= end_; ++iter) {
-                    *(iter - 1) = *iter;
-
-                }
-                array[size] = data_;
-                size++;
-
-            }
-        } else {
-            throw range_error("Negative/0 capacity");
-
-        }
-    }
-
-    void delLast() {
-        if (size > 0) {
-            size--;
-
-        } else {
-            throw range_error("Empty buffer");
-
-        }
-    }
-
-    T &last() {
-        if (size == 0) {
-            throw range_error("Empty buffer");
-
-        } else {
-            return *(end() - 1);
-        }
-
-    }
-
-    T &first() {
-        if (size == 0) {
-            throw range_error("Empty buffer");
-
-        } else {
-            return *begin();
-        }
-    }
-
-    T &at(unsigned index) {
-        if (size == 0) {
-            throw range_error("Empty buffer");
-
-        } else if (index >= size) {
-            throw range_error("Invalid index value");
-
-        } else {
-            return *(array + index);
-        }
-
-    }
-
-    T operator[](unsigned index) const {
-        if (size == 0) {
-            throw range_error("Empty buffer");
-
-        } else if (index >= size) {
-            throw range_error("Invalid index value");
-
-        } else {
-            return *(array + index);
-        }
-    }
-    //todo more information in exceptions
-    T &operator[](unsigned index) {
-        if (size == 0) {
-            throw range_error("Empty buffer");
-
-        } else if (index >= size) {
-            throw range_error("Invalid index value");
-
-        } else {
-            return *(array + index);
-        }
-    }
-
-    void changeCapacity(unsigned size_) {
-        if (size_ > 0) {
-            T *newArray = new T[size_]();
-            int new_size = min(size, size_);
-            for (int i = 0; i < new_size; i++) {
-                newArray[i] = array[i];
-            }
             delete[] array;
             array = newArray;
-            size = new_size;
-            capacity = size_;
-        } else {
+            head = 0;
+            size_ = new_size;
+            capacity = new_capacity;
+        }
+        else {
             throw range_error("Wrong value for capacity!");
         }
     }
